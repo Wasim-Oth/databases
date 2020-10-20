119ac1fc49ff16f016b57b0df8bb4df07d0967de
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-10-20 23:31:20 +0200

    added home work folder and created inside it ex-1.js and ex-2.js

--- Week1/homework/ex-1/meetup.js
+++ Week1/homework/ex-1/meetup.js
@@ -17,8 +17,10 @@ let Invitee_table = "create table Invitee (invitee_no int, invitee_name varchar(
 let Room_table = "create table Room (room_no int, room_name varchar(20), floor_number int)";
 let Meeting_table = "create table Meeting ( meeting_no int, meeting_title varchar(20), starting_time datetime, ending_time datetime, room_no int)";
 
+// values for the rows
 let Invitee_names = ['ahmed', 'kim', 'salam', 'rag', 'nor', 'wasim'];
-// console.log(Object.values(Invitee_values))
+let roo_name = ['a', 'b', 'c', 'd', 'e'];
+let meeting_title = ['time', 'work', 'learn', 'food', 'sport']
 
 
 function createDatabaseOrTable (x) {
@@ -28,21 +30,20 @@ connection.query(x, (error, results) => {
  })
 }
 
-
         // using the function to add the database and the tables
 
-// createDatabaseOrTable(meetup_database)
-// createDatabaseOrTable(Invitee_table)
-// createDatabaseOrTable(Room_table)
-// createDatabaseOrTable(Meeting_table)
+createDatabaseOrTable(meetup_database)
+createDatabaseOrTable(Invitee_table)
+createDatabaseOrTable(Room_table)
+createDatabaseOrTable(Meeting_table)
 
 
         // loop 5 times and in each time insert values to the tables 
 for ( let i = 1; i < 6; i++) {
     createDatabaseOrTable(`insert into Invitee values (${i}, '${Invitee_names[i-1]}', '${Invitee_names[5]}' )`);
-    createDatabaseOrTable(`insert into Room values (${i}, 'a', ${i} )`);
+    createDatabaseOrTable(`insert into Room values (${i}, '${roo_name[i-1]}', ${i} )`);
+    createDatabaseOrTable(`insert into Meeting values (${i}, '${meeting_title[i-1]}', '2020-0${i}-01 1${i}:00:00', '2020-0${i}-01 1${i+2}:00:00', ${i} )`);
 } 
-
 
 
         // drop evreything i created to try it again. uncomment  it we i need to use it
---
+++ Week1/homework/ex-2.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword',
+    database: 'world'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Connected");
+})
+  
+function excuteCommand(x){
+connection.query(x, (error, results) => {
+    if (error) throw error;
+    console.table(results)
+ })
+}
+
+        // 1- countries with population greater than 8 million
+excuteCommand("select * from city where Population > 8000000")
+
+        // 2- countries that have “land” in their names
+excuteCommand("select * from city where name like '%land%'")
+
+        // 3- cities with population in between 500,000 and 1 million
+excuteCommand("select * from city where Population > 500000 and Population < 1000000")
+
+        // 4- countries on the continent Europe
+excuteCommand("select name from country where Continent like 'Europe' ")
+        
+        // 5- countries in the descending order of their surface areas.
+excuteCommand("select name from country order by -SurfaceArea desc")
+
+        // 6-  names of all the cities in the Netherlands
+excuteCommand( "select name from city where CountryCode = 'NLD' ")
+
+        // 7-  population of Rotterdam
+excuteCommand( "select population from city where name = 'Rotterdam' ")
+
+        // 8- top 10 countries by Surface Area
+excuteCommand( "select name from country order by -SurfaceArea limit 10 ")
+
+        // 9-  10 most populated cities
+excuteCommand("select name from city order by Population desc limit 10 ")
+
+        // 10-  population number of the world
+excuteCommand(" select sum (population) from Country ")
+
+
+connection.end();
