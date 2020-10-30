673a8ec65664dbb13b4a08d42bd2ddcd4056aa5a
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-10-30 17:08:16 +0100

    Update ex-2.js

--- Week1/homework/ex-2.js
+++ Week1/homework/ex-2.js
@@ -26,10 +26,10 @@ excuteCommand("select * from city where Population > 8000000")
 excuteCommand("select * from city where name like '%land%'")
 
         // 3- cities with population in between 500,000 and 1 million
-excuteCommand("select * from city where Population > 500000 and Population < 1000000")
+excuteCommand("select * from city where Population between 500000 and 1000000")
 
         // 4- countries on the continent Europe
-excuteCommand("select name from country where Continent like 'Europe' ")
+excuteCommand("select name from country where Continent = 'Europe' ")
         
         // 5- countries in the descending order of their surface areas.
 excuteCommand("select name from country order by -SurfaceArea desc")
